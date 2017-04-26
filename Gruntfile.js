05d886fc063e5838592078ee67a4b7090416ccae
spriadka <spriadka@github.com>
2017-04-26 11:04:36 +0200

    testing coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,8 +16,18 @@ module.exports = function(grunt) {
           run: true
         }
       }
+    },
+    mocha_istanbul:{
+      coverage: {
+        src: 'lib/**/*.js',
+        options:{
+          mask: '*-spec.js'
+        }
+      }
     }
   });
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
   grunt.registerTask('test', ['eslint', 'mochaTest']);
   grunt.registerTask('default', ['test']);
+  grunt.registerTask('coverage', ['mocha_istanbul']);
 };
