f8397fc0976a198ef47ebeb4196f6cdc90afee93
spriadka <spriadka@github.com>
2017-05-11 10:37:26 +0200

    before test folder refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
+var sonarqubeScanner = require('sonarqube-scanner');
+
 module.exports = function(grunt) {
   'use strict';
-
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
     eslint: {
@@ -30,4 +31,21 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['eslint', 'mochaTest']);
   grunt.registerTask('default', ['test']);
   grunt.registerTask('coverage', ['mocha_istanbul']);
+  grunt.registerTask('sonar',function(cb){
+    sonarqubeScanner({
+      serverUrl : "https://metrics.skunkhenry.com",
+      token : "cm9iZXJ0OnNjb3R0aXNo",
+      options : {
+        "sonar.projectKey": "raincatcher",
+        "sonar.projectName": "fh-wfm",
+        "sonar.projectVersion": "0.2.3",
+        "sonar.sources":"./lib",
+        "sonar.language":"js",
+        "sonar.exclusions": "**/node_modules/**/*.js",
+        "sonar.login": "robert",
+        "sonar.password": "scottish",
+        "sonar.analysis.mode":"preview"
+ }
+    }, cb);
+  });
 };
